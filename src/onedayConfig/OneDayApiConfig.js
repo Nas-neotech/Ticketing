cb14894cf919431ccac5ea70413d2de01e60f9d0
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-15 16:07:10 +0300

    config file

--- src/onedayConfig/OneDayApiConfig.js
+++ src/onedayConfig/OneDayApiConfig.js
@@ -1,12 +1,14 @@
-const API_INFO = {
-  mainUrl: "https://api.1daycloud.com/v3/",
-  Authorization:
-    "dev_token_49xb4g53zv69jxgt670wp167ug5ulkxk849qq1wpg0ywkqpgdyffb5xpdkvutitqf8yd06f2f432yhrecaaj5zsbr6e9j31k018iwyc3nmtsa1zflqft4e",
-  TemplatesAppAuthorization:
-    "dev_token_2mx5cm7s7d2p2s8ugu40vabr6bv7g88gt7of8zkaa4vthy41a8iqy24e9h0i72z0ymu2ed0mp6ulr9ahe9fwhogcy67mq2xspdl27cgkg751zlbzk3pakx",
-  appId: 1547,
-  templatesSourceAppId: 2421,
-};
-export const ONEDAY_CONFIG = () => {
-  return API_INFO;
-};
+
+          const API_INFO = {
+              mainUrl: "https://api.1daycloud.com/v3/",
+              Authorization:
+                "dev_token_ioc181u0jh6tk6xxl5n60c3oowi34diakljht2f9o8z5e5h96vbashfrs9mil0fxwcwhqs8vq3zrzdw50dmgbo7p21rwfhs6ykf61j45ftjsopbtxo5ovx",
+              appId: 2712,
+              TemplatesAppAuthorization:
+              "dev_token_2mx5cm7s7d2p2s8ugu40vabr6bv7g88gt7of8zkaa4vthy41a8iqy24e9h0i72z0ymu2ed0mp6ulr9ahe9fwhogcy67mq2xspdl27cgkg751zlbzk3pakx",
+              templatesSourceAppId: 2421,
+            };
+            export const ONEDAY_CONFIG = () => {
+              return API_INFO;
+            };
+        
