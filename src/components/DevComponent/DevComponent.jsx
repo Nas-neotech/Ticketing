63bc5f15e7f97040d64da939b0d6b921c94bd5eb
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-16 10:27:10 +0300

    Update DevComponent.jsx

--- src/components/DevComponent/DevComponent.jsx
+++ src/components/DevComponent/DevComponent.jsx
@@ -3,7 +3,14 @@ import usePage from "../../contexts/usePage/usePage";
 import ElementSettingsWrapper from "../ElementTypes/ElementSettingsWrapper/ElementSettingsWrapper";
 import TestDevComponent from "./TestDevComponent";
 
-const DevComponent = ({ drag, element, isLayoutEle, parent, dragPreview, item }) => {
+const DevComponent = ({
+  drag,
+  element,
+  isLayoutEle,
+  parent,
+  dragPreview,
+  item,
+}) => {
   const { isView, setSelectedElement, selectedElement } = usePage();
   const ref = useRef();
 
@@ -79,19 +86,21 @@ const DevComponent = ({ drag, element, isLayoutEle, parent, dragPreview, item })
             Search
           </button>
         </div>
-      ) : element?.text === "Result" ?  <div className="max-w-xs mx-auto bg-white shadow-lg rounded-2xl p-6 flex flex-col items-center space-y-3">
-      {/* Avatar */}
-      <div className="w-16 h-16 rounded-full bg-blue-100 flex items-center justify-center">
-        {/* <FaUser className="text-blue-600 text-2xl" /> */}
-        icon
-      </div>
+      ) : element?.text === "Result" ? (
+        <div className="max-w-xs mx-auto bg-[rgb(55,16,195)] shadow-lg rounded-2xl p-6 flex flex-col items-center space-y-3">
+          {/* Avatar */}
+          <div className="w-16 h-16 rounded-full bg-blue-100 flex items-center justify-center">
+            {/* <FaUser className="text-blue-600 text-2xl" /> */}
+            icon
+          </div>
 
-      {/* User Info */}
-      <div className="text-center">
-        <h3 className="text-lg font-semibold text-gray-800">name</h3>
-        <p className="text-gray-500 text-sm">phone</p>
-      </div>
-    </div>:(
+          {/* User Info */}
+          <div className="text-center">
+            <h3 className="text-lg font-semibold text-gray-800">name</h3>
+            <p className="text-gray-500 text-sm">phone</p>
+          </div>
+        </div>
+      ) : (
         <p>{`This is Dev Component: id => ${element?.text || element?.id}`}</p>
       )}
     </div>
