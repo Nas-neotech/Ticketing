7517aada7e97ee7acdd7a728ccfda21d2e631c7d
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-16 14:36:05 +0300

    Update SearchCompo.jsx

--- src/components/SearchCompo.jsx
+++ src/components/SearchCompo.jsx
@@ -43,7 +43,7 @@ const SearchCompo = ({ onSearchResults }) => {
   return (
     <div className="max-w-md mx-auto bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-2xl p-6 flex flex-col gap-6">
       <h2 className="text-2xl font-semibold text-gray-800 text-center">
-        Search test data icon
+        Search test data icon 2
       </h2>
 
       {fields.map(({ id, type, placeholder }) => (
