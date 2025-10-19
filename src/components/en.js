7469777d7389230428ab1dcd6c505502df631db7
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-19 11:18:26 +0300

    Create en.js

---
+++ src/components/en.js
@@ -0,0 +1,17 @@
+{
+    "User_Info": "User Information",
+    "Enter_Credentials": "Enter Credentials",
+    "form": {
+        "placeholders": {
+        "name": "Enter name",
+        "phone": "Enter phone number",
+        "mobile": "Enter mobile number",
+        "port": "Enter port serial number"
+        },
+        "SearchBTN": "Search"
+     },
+    "No_Results": "No Results Found",
+    "View_Details": "View Details",
+    "Unknown":"Unknown"
+     
+}
