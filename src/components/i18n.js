bf0f692c1d8afb5f5a6dc2b5b1c6de2e3e143720
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-19 11:19:02 +0300

    Update i18n.js

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "../Languges/en.json";
-import ar from "../Languges/ar.json";
+import en from "../en.json";
+import ar from "../ar.json";
 
 i18n.use(initReactI18next).init({
   resources: {
