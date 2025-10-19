8eea26e826b936c92a321ba472ecc46fdcb7bdba
Nas-neotech <r.alsaegh@neotech-s.com>
2025-10-19 11:16:18 +0300

    Create i18n.js

---
+++ src/components/i18n.js
@@ -0,0 +1,17 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "../Languges/en.json";
+import ar from "../Languges/ar.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    EN: { translation: en },
+    AR: { translation: ar },
+  },
+  lng: "EN",
+  fallbackLng: "EN",
+  interpolation: { escapeValue: false },
+});
+
+export default i18n;
