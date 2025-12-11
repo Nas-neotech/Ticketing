20e98a7f8b2572cd92b83f94f545a144a3b98bd5
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-11 12:48:58 +0300

    Update DevComponent.jsx

--- src/components/DevComponent/DevComponent.jsx
+++ src/components/DevComponent/DevComponent.jsx
@@ -25,7 +25,6 @@ const DevComponent = ({
   return (
     <div
       ref={ref}
-      className="bg-[url(./src/assets/images/bg.png)] bg-cover"
       style={{
         position: "relative",
         ...(selectedElement?.id === element?.id
