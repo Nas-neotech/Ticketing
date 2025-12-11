66b7f5e5afca095f79dfc0427a24dcd4bcb4eb35
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-11 16:40:13 +0300

    Update SearchCompo.jsx

--- src/components/SearchCompo.jsx
+++ src/components/SearchCompo.jsx
@@ -166,7 +166,7 @@ const SearchCompo = ({ onSearchResults }) => {
                      rounded-xl"
             >
               <SearchOutlined style={{ fontSize: "24px" }} />
-              Search
+              Searchsssss
             </button>
           </div>
         </div>
