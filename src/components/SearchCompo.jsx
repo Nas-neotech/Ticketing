c8193557b2b534d84b9f6cd1cc5ea1751e06f677
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-14 16:50:56 +0300

    Update SearchCompo.jsx

--- src/components/SearchCompo.jsx
+++ src/components/SearchCompo.jsx
@@ -9,7 +9,7 @@ import {
 import ResultCompo from "./ResultCompo";
 import background from "../assets/images/NasBg.png";
 import logo from "../assets/images/NasLogo.gif";
-import { t } from "i18next";
+import { t } from "i18next"
 import useLang from "../contexts/useLanguage/useLang";
 
 const SearchCompo = () => {
@@ -21,7 +21,7 @@ const SearchCompo = () => {
       {
         id: "name",
         type: "text",
-        placeholder: t("name"),
+        placeholder: t('name'),
         icon: UserOutlined,
       },
       {
