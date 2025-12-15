09c0243f479251194b4effce429d5d7e07cee682
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-15 10:49:36 +0300

    Update SearchCompo.jsx

--- src/components/SearchCompo.jsx
+++ src/components/SearchCompo.jsx
@@ -27,21 +27,21 @@ const SearchCompo = () => {
       {
         id: "phone",
         type: "text",
-        placeholder: "Phone number newwww",
+        placeholder: t('Phone number'),
         icon: PhoneOutlined,
         onlyNumbers: true,
       },
       {
         id: "mobile",
         type: "text",
-        placeholder: "Mobile number",
+        placeholder: t('Mobile number'),
         icon: MobileOutlined,
         onlyNumbers: true,
       },
       {
         id: "port",
         type: "text",
-        placeholder: "Port's serial number",
+        placeholder: t('Port's serial number'),
         icon: ClusterOutlined,
         onlyNumbers: true,
       },
@@ -161,7 +161,7 @@ const SearchCompo = () => {
               className="w-full flex bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-900 shadow-lg p-2 items-center justify-center gap-2 text-white font-semibold rounded-xl"
             >
               <SearchOutlined style={{ fontSize: "24px" }} />
-              Search
+              {t('Search')}
             </button>
           </div>
         </div>
