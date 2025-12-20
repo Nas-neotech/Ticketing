f40078933fce7a9510817b70afbe9d477acbebb1
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-20 15:14:32 +0300

    Update ResultCompo.jsx

--- src/components/ResultCompo.jsx
+++ src/components/ResultCompo.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import {
   UserOutlined,
   PhoneOutlined,
@@ -6,71 +6,78 @@ import {
   ClusterOutlined,
 } from "@ant-design/icons";
 import { t } from "i18next";
-import useLang from "../contexts/useLanguage/useLang";
 
 const ResultCompo = ({ data }) => {
   const [activeUserId, setActiveUserId] = useState(null);
-  const { lang } = useLang();
 
   if (data.length === 0) {
-    alert("No results found");
+    return (
+      <div className="text-red-600 font-bold text-lg">{t("no user found")}</div>
+    );
   }
 
   return (
-    <div className="w-full flex flex-wrap justify-center gap-6  ">
-      {data.map((user) => {
+    <div className="w-full flex flex-wrap justify-center gap-6">
+      {data.map((user, idx) => {
         const info = [
           { id: "phone", icon: PhoneOutlined, value: user.phone },
-          { id: "mobile", icon: MobileOutlined, value: user.mobile },
-          { id: "port", icon: ClusterOutlined, value: user.port },
+          {
+            id: "mobile",
+            icon: MobileOutlined,
+            value: user.mobile || user.gsm,
+          },
+          {
+            id: "port",
+            icon: ClusterOutlined,
+            value: user.adsl_port || user.port,
+          },
         ];
+
         return (
           <div
-            key={user.id}
-            className="w-[15%] bg-gradient-to-br from-white to-blue-50
-           shadow-lg shadow-black transition-shadow duration-300
-            rounded-2xl p-4 gap-3 flex flex-col items-center border border-gray-100"
+            key={idx}
+            className="w-[15%] bg-gradient-to-br from-white to-blue-50 shadow-lg shadow-black
+                       rounded-2xl p-4 gap-3 flex flex-col items-center border border-gray-100"
           >
             <div className="flex w-full items-center justify-center gap-3">
               <div
                 className="w-10 h-10 rounded-full flex items-center justify-center
-                  bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-900"
+                           bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-900"
               >
-                <UserOutlined className="text-white text-2xl " />
+                <UserOutlined className="text-white text-2xl" />
               </div>
-              <div className="text-lg font-semibold text-gray-800 ">
-                {user.name || "Unknown"}
+              <div className="text-lg font-semibold text-gray-800">
+                {user.first && user.last
+                  ? `${user.first} ${user.last}`
+                  : user.username || "Unknown"}
               </div>
             </div>
 
-            <div className="flex flex-col w-full items-center gap-1 text-center">
-              {info.map((i, index) => {
-                return (
-                  <div
-                    key={i.id}
-                    className="text-gray-600 w-full font-bold flex items-center"
-                  >
-                    <div className="w-[15%]">
-                      <i.icon
-                        className="text-lg "
-                        style={{ transform: "rotateY(180deg)" }}
-                      />
-                    </div>
-                    <div className="w-[85%]">{i.value || "-"}</div>
+            <div className="flex flex-col w-full items-center gap-1 text-center mt-3">
+              {info.map((i) => (
+                <div
+                  key={i.id}
+                  className="text-gray-600 w-full font-bold flex items-center"
+                >
+                  <div className="w-[15%]">
+                    <i.icon className="text-lg" />
                   </div>
-                );
-              })}
+                  <div className="w-[85%]">{i.value || "-"}</div>
+                </div>
+              ))}
             </div>
 
             <button
               onClick={() => {
-                setActiveUserId(user.id);
+                setActiveUserId(idx);
                 setTimeout(() => setActiveUserId(null), 100);
               }}
               className={`w-full flex bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-900 
-                   p-2 items-center justify-center gap-2 text-white font-semibold shadow-black
-                   rounded-xl transition-shadow duration-100
-                 ${activeUserId === user.id ? "shadow-inner" : "shadow-lg"}`}
+                          p-2 items-center justify-center gap-2 text-white font-semibold shadow-black
+                          rounded-xl transition-shadow duration-100
+                          ${
+                            activeUserId === idx ? "shadow-inner" : "shadow-lg"
+                          }`}
             >
               {t("account info")}
             </button>
