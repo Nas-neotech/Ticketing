9593a9430b08596bcdb60531bbd65a4bccc2e72d
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-20 15:25:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { API } from "./src/API_URL";
+import { API } from "./API_URL";
 
 export default defineConfig({
   plugins: [react()],
