eada066aade7c668cdf50bacc4b58f63dbb8ace5
Nas-neotech <r.alsaegh@neotech-s.com>
2025-12-20 15:23:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { API } from "./API_URL";
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { API } from "./src/API_URL";
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       "/freeside": {
         target: `${API}`,
